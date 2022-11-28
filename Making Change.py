40cd4a1e5e8723cf8893e59d7393a5aea0e424f2
WilliamFTinoco <115164194+WilliamFTinoco@users.noreply.github.com>
2022-11-28 14:38:46 +0000

    Update Making Change.py

--- Making Change.py
+++ Making Change.py
@@ -1,1 +1,16 @@
-def makingChange():
+def makingChange(units):
+  if units == 0:
+    return(0)
+  else:
+    coins = 0
+    while units > 0:
+      if units >= 500:
+        units += -500
+        coins += 1
+      else if units >= 100:
+        units += -100
+        coins += 1
+      else if units >= 25:
+        units += -25
+        coins += 1
+      
