05b6d1d42f15f318c037d33f660042d790147310
WilliamFTinoco <115164194+WilliamFTinoco@users.noreply.github.com>
2022-12-05 10:27:09 +0000

    Update Roman Numeral Comparison.py

--- Roman Numeral Comparison.py
+++ Roman Numeral Comparison.py
@@ -1,1 +1,22 @@
-class numcompare:
+class nums:
+  def __init__(self, num_a, num_b):
+    self.dict = {
+      "M":1000,
+      "D":500,
+      "C":100,
+      "L":50,
+      "X":10,
+      "V":5,
+      "I":1
+      }
+    self.a = 0
+    self.b = 0
+    for x in num_a:
+      self.a += self.dict[x]
+    for x in num_b:
+      self.b += self.dict[x]
+  def numcompare(self):
+    if self.a < self.b:
+      return("True")
+    else:
+      return("False")
